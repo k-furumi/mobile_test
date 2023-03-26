7f463f4e2f9e356c9b6d3bc07887b177d975f71b
k furumi <furumikouki@gmail.com>
2023-03-26 15:35:02 +0900

    vuetify導入

--- mobile_test/nuxt.config.ts
+++ mobile_test/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false, // サーバーは使用不可
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+    },
 })
